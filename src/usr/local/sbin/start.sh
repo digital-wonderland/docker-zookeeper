6d1393150fb8fb1433326ed14d30ba9b923cbe8e
Stephan Kleine <stephan.kleine@gmail.com>
2014-11-16 21:39:04 +0100

    make Zookeeper id configurable via $ZOOKEEPER_ID

--- src/usr/local/sbin/start.sh
+++ src/usr/local/sbin/start.sh
@@ -1,6 +1,19 @@
 #! /usr/bin/env bash
 
-ZOOKEEPER_ID="${HOST_IP##*.}"
+# Fail hard and fast
+set -eo pipefail
+
+if [ -z "$HOST_IP" ]; then
+  echo "\$HOST_IP not set"
+  exit -1
+else
+  echo "HOST_IP=$HOST_IP"
+fi
+
+if [ -z "$ZOOKEEPER_ID" ]; then
+  ZOOKEEPER_ID="${HOST_IP##*.}"
+fi
+echo "ZOOKEEPER_ID=$ZOOKEEPER_ID"
 
 echo $ZOOKEEPER_ID > /var/lib/zookeeper/myid
 
